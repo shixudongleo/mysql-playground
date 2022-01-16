368fcf2e9e460452ecd97908dd59fcde748f655c
shixudongleo <shixudongleo@gmail.com>
2022-01-16 07:09:29 +0000

    Add chapter3 exercise solution

---
+++ chapter3.sql
@@ -0,0 +1,35 @@
+-- exercise 3-1
+SELECT actor_id,
+  first_name,
+  last_name
+FROM actor
+ORDER BY last_name,
+  first_name;
+
+-- exercise 3-2
+SELECT actor_id,
+  first_name,
+  last_name
+FROM actor
+WHERE last_name IN ('WILLIAMS', 'DAVIS');
+
+SELECT actor_id,
+  first_name,
+  last_name
+FROM actor
+WHERE last_name = 'WILLIAMS'
+  OR last_name = 'DAVIS';
+
+-- exercise 3-3
+SELECT DISTINCT customer_id
+FROM rental
+WHERE DATE(rental_date) = '2005-07-05';
+
+-- exercise 3-4
+SELECT c.email,
+  r.return_date
+FROM customer AS c
+  INNER JOIN rental AS r ON c.customer_id = r.customer_id
+WHERE DATE(r.rental_date) = '2005-06-14'
+ORDER BY r.return_date DESC,
+  c.email;
